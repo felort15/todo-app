d2ecc3713b1117854c65eba6cd2040824575d250
Felipe Ortiz <felort15@gmail.com>
2025-03-22 21:37:13 -0600

    Fix 404 routing issues

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     unoptimized: true
-  }
+  },
+  // Enable trailing slashes for better compatibility
+  trailingSlash: true
 }
 
 module.exports = nextConfig 
